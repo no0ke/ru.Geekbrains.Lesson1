531b244bc1acac872f5ced2534eaab272827262d
Sergey <no0ke@yandex.ru>
2020-11-30 15:26:16 +0300

    ДЗ 4 урока Java2

---
+++ Java2/src/main/java/lesson4/Run.java
@@ -0,0 +1,7 @@
+package lesson4;
+
+public class Run {
+    public static void main(String[] args){
+        new Window();
+    }
+}
---
+++ Java2/src/main/java/lesson4/Window.java
@@ -0,0 +1,56 @@
+package lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    private JTextField field;
+    private final JTextArea jta;
+    private JButton button;
+
+    public Window(){
+        setTitle("Simple chat");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(100, 100, 500, 500);
+
+        JPanel jp = new JPanel();
+        jp.setLayout(new BorderLayout());
+
+        jta = new JTextArea();
+        jta.setLineWrap(true);
+        JScrollPane jsp = new JScrollPane(jta);
+        jp.add(jsp, BorderLayout.CENTER);
+
+        JPanel bottomJp = new JPanel();
+        bottomJp.setLayout(new BorderLayout());
+
+        addTextField();
+        bottomJp.add(field, BorderLayout.CENTER);
+
+        addButton();
+        bottomJp.add(button, BorderLayout.SOUTH);
+        jp.add(bottomJp, BorderLayout.SOUTH);
+        add(jp);
+
+        setVisible(true);
+        field.grabFocus();
+    }
+
+    private void sendMessage(){
+        if(!field.getText().isEmpty()){
+            jta.append(field.getText()+"\n");
+            field.setText("");
+            field.grabFocus();
+        }
+    }
+
+    private void addTextField(){
+        field = new JTextField();
+        field.addActionListener(e -> sendMessage());
+    }
+
+    private void addButton(){
+        button = new JButton("Отправить");
+        button.addActionListener(e -> sendMessage());
+    }
+}
