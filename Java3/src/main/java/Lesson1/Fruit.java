dd23dadbf774797a10a1a4b367f4aa3acd032152
Sergey <no0ke@yandex.ru>
2020-12-21 19:55:43 +0300

    ДЗ 7 урока Java2

---
+++ Java3/src/main/java/Lesson1/Fruit.java
@@ -0,0 +1,5 @@
+package Lesson1;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
---
+++ Java3/src/main/java/Lesson1/Orange.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Orange extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.5f;
+    }
+}
